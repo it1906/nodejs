e44c30a8b1972693b44595292a597467f1ad26e8
unknown <githublmao@seznam.cz>
2021-03-15 22:19:13 +0100

    zaklad js

---
+++ app.js
@@ -0,0 +1,92 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    const userGrid = document.querySelector('.grid-user');
+    const computerGrid = document.querySelector('.grid-computer');
+    const displayGrid = document.querySelector('.grid-display');
+    const ships = document.querySelector('.ship');
+    const destroyer = document.querySelector('.destroyer-container');
+    const submarine = document.querySelector('.submarine-container');
+    const cruiser = document.querySelector('.cruiser-container');
+    const battleship = document.querySelector('.battleship-container');
+    const carrier = document.querySelector('.carrier-container');
+    const startButton = document.querySelector('#start');
+    const rotateButton = document.querySelector('#rotate');
+    const turnButton = document.querySelector('#whose-go');
+    const infoDisplay = document.querySelector('#info');
+    const userSquares = [];
+    const computerSquares = [];
+
+    const width = 10;
+
+    //Tvorba desky
+    function createBoard(grid,squares,width){
+        for(let i =0;i <width*width;i++){
+        const square = document.createElement('div')
+        square.dataset.id = i
+        grid.appendChild(square)
+        squares.push(square)
+        }
+    }
+    createBoard(userGrid, userSquares)
+    createBoard(computerGrid, computerSquares)
+
+    //Lodicky
+    const shipArray = [
+        {
+            name:'destroyer',
+            directions:[
+                [0,1],
+                [0,width]
+            ]
+        },
+        {
+            name:'submarine',
+            directions:[
+                [0,1,2],
+                [0,width, width*2]
+            ]
+        },
+        {
+            name:'cruiser',
+            directions:[
+                [0,1,2],
+                [0,width,width*2]
+            ]
+        },
+        {
+            name:'battleship',
+            directions:[
+                [0,1,2,3],
+                [0,width,width*2,width*3]
+            ]
+        },
+        {
+            name:'carrierr',
+            directions:[
+                [0,1,2,3,4],
+                [0,width,width*2,width*3,width*4]
+            ]
+        }
+    ]
+
+    //random kreslime lodicky
+    function generate (ship){
+        let randomDirecton = Math.floor(Math.random()*ship.directions.length)
+        let current = ship.directions[randomDirecton]
+        if(randomDirecton ===0) direction = 1
+        if(randomDirecton ===1) direction = 10
+        let randomStart = Math.abs(Math.floor(Math.random()*computerSquares.length -(ship.directions[0].length*direction)))
+
+        const isTaken = current.some(index => computerSquares[randomStart + index].classList.contains('taken'))
+        const isAtRightEdge = current.some(index=>(randomStart+index)%width === width - 1)
+        const isAtLeftEdge = current.some(index=>(randomStart+index)%width === 0)
+
+        if(!isTaken && !isAtRightEdge && !isAtLeftEdge) current.forEach(index => computerSquares[randomStart+index].classList.add('taken',ship.name))
+        else generate(ship)
+    }
+
+    generate(shipArray[0])
+    generate(shipArray[1])
+    generate(shipArray[2])
+    generate(shipArray[3])
+    generate(shipArray[4])
+});
