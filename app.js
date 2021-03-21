6604561c63f65b9d92ac7c23b93e489ccc054a98
unknown <githublmao@seznam.cz>
2021-03-21 21:06:34 +0100

    prvni tutorial hotov...na 99%

--- app.js
+++ app.js
@@ -10,11 +10,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const carrier = document.querySelector('.carrier-container');
     const startButton = document.querySelector('#start');
     const rotateButton = document.querySelector('#rotate');
-    const turnButton = document.querySelector('#whose-go');
+    const turnDisplay = document.querySelector('#whose-go');
     const infoDisplay = document.querySelector('#info');
     const userSquares = [];
     const computerSquares = [];
     let isHorizontal = true;
+    let isGameOver = false;
+    let currentPlayer = 'user';
 
     const width = 10;
 
@@ -111,7 +113,7 @@ document.addEventListener('DOMContentLoaded', () => {
             isHorizontal = true
             return
         }
-    }rotateButton.addEventListener('click', rotate)
+    } rotateButton.addEventListener('click', rotate)
 
     //pohyb lodicek na pole
     ships.forEach(ship => ship.addEventListener('dragstart', dragStart))
@@ -151,22 +153,143 @@ document.addEventListener('DOMContentLoaded', () => {
         let lastShipIndex = parseInt(shipNameWithLastId.substr(-1));
         let shipLastId = lastShipIndex + parseInt(this.dataset.id);
         console.log(shipLastId)
+        const notAllowedHorizontal = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 1, 11, 21, 31, 41, 51, 61, 71, 81, 91, 2, 22, 32, 42, 52, 62, 72, 82, 92, 3, 13, 23, 33, 43, 53, 63, 73, 83, 93]
+        const notAllowedVertical = [99, 98, 97, 96, 95, 94, 93, 92, 91, 90, 89, 88, 87, 86, 85, 84, 83, 82, 81, 80, 79, 78, 77, 76, 75, 74, 73, 72, 71, 70, 69, 68, 67, 66, 65, 64, 63, 62, 61, 60]
+        let newNotAllowedHorizontal = notAllowedHorizontal.splice(0, 10 * lastShipIndex)
+        let newNotAllowedVertical = notAllowedVertical.splice(0, 10 * lastShipIndex)
+
 
         selectedShipIndex = parseInt(selectedShipNameWithIndex.substr(-1))
 
         shipLastId = shipLastId - selectedShipIndex;
 
-        if (isHorizontal) {
+        if (isHorizontal && !newNotAllowedHorizontal.includes(shipLastId)) {
             for (let i = 0; i < draggedShipLength; i++) {
                 userSquares[parseInt(this.dataset.id) - selectedShipIndex + i].classList.add('taken', shipClass)
             }
-        } else if (!isHorizontal) {
+        } else if (!isHorizontal && !newNotAllowedVertical.includes(shipLastId)) {
             for (let i = 0; i < draggedShipLength; i++) {
-                userSquares[parseInt(this.dataset.id) - selectedShipIndex + width*i].classList.add('taken', shipClass)
+                userSquares[parseInt(this.dataset.id) - selectedShipIndex + width * i].classList.add('taken', shipClass)
             }
-        }else return
+        } else return
 
         displayGrid.removeChild(draggedShip)
     }
-    function dragEnd() { }
+    function dragEnd() {
+    }
+    function playGame() {
+        if (isGameOver) return
+        if (currentPlayer === 'user') {
+            turnDisplay.innerHTML = 'Your Go'
+            computerSquares.forEach(square => square.addEventListener('click', function (e) {
+                revealSquare(square)
+            }))
+        }
+        if (currentPlayer === 'computer') {
+            turnDisplay.innerHTML = 'Computers Go'
+            setTimeout(computerGo, 1000)
+        }
+    }
+    startButton.addEventListener('click', playGame)
+
+    let destroyerCount = 0;
+    let submarineCount = 0;
+    let cruiserCount = 0;
+    let battleshipCount = 0;
+    let carrierCount = 0;
+
+    function revealSquare(square) {
+        if (!square.classList.contains('boom')) {
+            if (square.classList.contains('destroyer')) destroyerCount++
+            if (square.classList.contains('submarine')) submarineCount++
+            if (square.classList.contains('cruiser')) cruiserCount++
+            if (square.classList.contains('battleship')) battleshipCount++
+            if (square.classList.contains('carrier')) carrierCount++
+        }
+        if (square.classList.contains('taken')) {
+            square.classList.add('boom')
+        } else {
+            square.classList.add('miss')
+        }
+        checkForWins()
+        currentPlayer = 'computer'
+        playGame()
+    }
+    let cpuDestroyerCount = 0;
+    let cpuSubmarineCount = 0;
+    let cpuCruiserCount = 0;
+    let cpuBattleshipCount = 0;
+    let cpuCarrierCount = 0;
+
+    function computerGo() {
+        let random = Math.floor(Math.random() * userSquares.length)
+        if (!userSquares[random].classList.contains('boom')) {
+            userSquares[random].classList.add('boom')
+            if (userSquares[random].classList.contains('destroyer')) cpuDestroyerCount++
+            if (userSquares[random].classList.contains('submarine')) cpuSubmarineCount++
+            if (userSquares[random].classList.contains('cruiser')) cpuCruiserCount++
+            if (userSquares[random].classList.contains('battleship')) cpuBattleshipCount++
+            if (userSquares[random].classList.contains('carrier')) cpuCarrierCount++
+            checkForWins()
+        } else computerGo()
+        currentPlayer = 'user'
+        turnDisplay.innerHTML = 'Your Go'
+    }
+
+    function checkForWins() {
+        if (destroyerCount === 2) {
+            infoDisplay.innerHTML = 'You sunk the enemy destroyer'
+            destroyerCount = 10
+        }
+        if (submarineCount === 3) {
+            infoDisplay.innerHTML = 'You sunk the enemy submarine'
+            submarineCount = 10
+        }
+        if (cruiserCount === 3) {
+            infoDisplay.innerHTML = 'You sunk the enemy cruiser'
+            cruiserCount = 10
+        }
+        if (battleshipCount === 4) {
+            infoDisplay.innerHTML = 'You sunk the enemy battleship'
+            battleshipCount = 10
+        }
+        if (carrierCount === 5) {
+            infoDisplay.innerHTML = 'You sunk the enemy carrier'
+            carrierCount = 10
+        }
+        if (cpuDestroyerCount === 2) {
+            infoDisplay.innerHTML = 'Destroyer lost'
+            cpuDestroyerCount = 10
+        }
+        if (cpuSubmarineCount === 3) {
+            infoDisplay.innerHTML = 'Submarine lost'
+            cpuSubmarineCount = 10
+        }
+        if (cpuCruiserCount === 3) {
+            infoDisplay.innerHTML = 'Cruiser lost'
+            cpuCruiserCount = 10
+        }
+        if (cpuBattleshipCount === 4) {
+            infoDisplay.innerHTML = 'Battleship lost'
+            cpuBattleshipCount = 10
+        }
+        if (cpuCarrierCount === 5) {
+            infoDisplay.innerHTML = 'Carrier lost'
+            cpuCarrierCount = 10
+        }
+        if((destroyerCount+submarineCount+cruiserCount+battleshipCount+carrierCount) ===  50){
+            infoDisplay.innerHTML = "YOU WON"
+            gameOver()
+        }
+        if((cpuDestroyerCount+cpuSubmarineCount+cpuCruiserCount+cpuBattleshipCount+cpuCarrierCount) ===  50){
+            infoDisplay.innerHTML = "YOU LOST"
+            gameOver()
+        }
+    }
+
+    function gameOver(){
+        isGameOver = true
+        startButton.removeEventListener('click', playGame)
+    }
+
 })
